2820941e95072e7b9f5bc8849adf1d54d23fb883
PaulBertLandicho <141722252+PaulBertLandicho@users.noreply.github.com>
2024-10-23 05:14:04 -0700

    Update GroceryItem.js

--- myproject/src/components/GroceryItem.js
+++ myproject/src/components/GroceryItem.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { FaListAlt } from 'react-icons/fa'; // Import the list icon
+import './css/GroceryItem.css';
 
-import './css/GroceryItem.css'
-
-const GroceryItem = ({ item, onEdit, onDelete }) => {
+const GroceryItem = ({ item, onEdit, onDelete, onAddToCart }) => {
   const getImageSrc = () => {
     if (item.image instanceof File) {
       return URL.createObjectURL(item.image);
@@ -13,20 +13,27 @@ const GroceryItem = ({ item, onEdit, onDelete }) => {
   return (
     <div className="grocery-item">
 
-    {/* Display the image */}
-    {item.image ? (
-      <img src={getImageSrc()} alt={item.name} className="grocery-item-image" style={{ width: '100px', height: '100px' }} />
-    ) : (
-      <div className="grocery-item-placeholder">No Image</div>
-    )}
-    <div>
-      <h4>{item.name}</h4>
-      <p>Price: ₱{item.price ? Number(item.price).toFixed(2) : '0.00'}</p> 
-      <button className="edit" onClick={() => onEdit(item)}>Edit</button>
-      <button className="delete" onClick={() => onDelete(item.id)}>Delete</button>
+      {/* List icon in top right corner */}
+      <div className='grocery'>
+      <div className="add-to-cart-icon" onClick={() => onAddToCart(item)}>
+        <FaListAlt size={30} color="darkgreen" />
+      </div>
+      </div>
+      
+      {/* Display the image */}
+      {item.image ? (
+        <img src={getImageSrc()} alt={item.name} className="grocery-item-image" style={{ width: '150px', height: '120px' }} />
+      ) : (
+        <div className="grocery-item-placeholder">No Image</div>
+      )}
+      
+      <div>
+        <h4>{item.name}</h4>
+        <p>Price: ₱{item.price ? Number(item.price).toFixed(2) : '0.00'}</p> 
+        <button className="edit" onClick={() => onEdit(item)}>Edit</button>
+        <button className="delete" onClick={() => onDelete(item.id)}>Delete</button>
+      </div>
     </div>
-  </div>
-  
   );
 };
 
